9f35f8d36fac65166cc6adc0273b138fe483931f
flpelerin <florianpelerin110304@gmail.com>
2024-08-04 18:02:22 +0000

    source update

---
+++ mamba_trainer/utils/time.py
@@ -0,0 +1,79 @@
+import time
+
+import mamba_trainer.utils.metaclass
+
+
+class Time(metaclass=CallableMeta):
+    time_init = None
+    time_last = None
+
+
+    @staticmethod
+    def __call__():
+        return Time.Get()
+
+
+    @staticmethod
+    def Get():
+        time_current = time.time()
+
+        if Time.time_init == None:
+            Time.time_init = time_current
+
+        if Time.time_last == None:
+            Time.time_last = time_current
+
+        return time_current
+
+
+    @staticmethod
+    def FormatString(number):
+        number = round(number)
+
+        hours = number // 3600
+        minutes = (number % 3600) // 60
+        seconds = number % 60
+
+        time_string = ""
+
+        if hours > 0:
+            time_string += str(hours) + "h "
+        if minutes > 0 or hours > 0:
+            time_string += str(minutes) + "m "
+        time_string += str(seconds) + "s"
+
+        return time_string
+
+
+    @staticmethod
+    def FormatSecond(time_string):
+        number = 0
+        parts = time_string.split()
+
+        for part in parts:
+            if part.endswith('h'):
+                number += int(part[:-1]) * 3600
+            elif part.endswith('m'):
+                number += int(part[:-1]) * 60
+            elif part.endswith('s'):
+                number += int(part[:-1])
+
+        return number
+
+
+    @staticmethod
+    def Up(raw=False):
+        time_current = Time()
+        time_up = time_current - Time.time_init
+
+        return time_up if raw is True else Time.FormatString(time_up)
+
+
+    @staticmethod
+    def Step(raw=False):
+
+        time_current = Time()
+        time_step = time_current - Time.time_last
+        Time.time_last = time_current
+
+        return time_step if raw is True else Time.FormatString(time_step)
---
+++ mamba_trainer/utils/wandb.py
@@ -0,0 +1,51 @@
+import os
+import wandb
+
+import mamba_trainer.utils.metaclass
+
+
+class Wandb(metaclass=CallableMeta):
+    wandb_has_init = False
+
+    @staticmethod
+    def Init():
+        if Globals.wandb_log_run is False:
+            return
+
+        if Wandb.wandb_has_init is True:
+            return
+
+        Wandb.wandb_has_init = True
+
+        project  = Globals.wandb_project
+        entity   = Globals.wandb_entity
+        api_key  = Globals.wandb_api_key
+        name     = Globals.wandb_name
+
+        if name is None or name == "":
+            name = "run-" + Util.RandomCode()
+
+        os.environ["WANDB_API_KEY"] = api_key
+
+        wandb.init(project=project, entity=entity, name=name)
+
+
+    @staticmethod
+    def Log(args):
+        if Globals.wandb_log_run is False:
+            return
+
+        if Wandb.wandb_has_init is False:
+            Wandb.Init()
+
+        if Wandb.wandb_has_init is True:
+            wandb.log(args)
+
+
+    @staticmethod
+    def Finish():
+        if Globals.wandb_log_run is False:
+            return
+
+        if Wandb.wandb_has_init is True:
+            wandb.finish()
