c17233b77cc65d01c6f5d0d10578466af1e6e9b6
flpelerin <florianpelerin110304@gmail.com>
2024-08-04 17:45:34 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="example_project",
+    name="mamba_trainer",
     version="0.1",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'example_project= example_project.main:hello_world',
-        ],
-    },
 )
